88f1ee2b3afc3f8c3a5751c80cba4f7efa904aa3
iwaneshuhei <iwane.shuhei@gmail.com>
2021-08-31 00:20:47 +0900

    alpha-0830 protoimpl frameNo

--- src/web/views/task-three/fl-cubes.tsx
+++ src/web/views/task-three/fl-cubes.tsx
@@ -15,9 +15,20 @@ const FLCubes = React.forwardRef<Object3D, Props>(({ frameNo, annotations, showL
     return (
         <group ref={ref}>
             {annotations.map(a => {
-                const points = a.points[frameNo as any];
+                let points = a.points[frameNo as any];
                 if (points) {
                     return (<FLCube key={a.id} id={a.id} points={points} showLabel={showLabel} color={a.color} onClick={onClick} />);
+                } else {
+                    const res = Object.keys(a.points).reduce<{ last: string }>((r, f) => {
+                        if (f < frameNo) {
+                            r.last = f;
+                        }
+                        return r;
+                    }, { last: '0001' });
+                    if (a.points[res.last]) {
+                        points = a.points[res.last];
+                        return (<FLCube key={a.id} id={a.id} points={points} showLabel={showLabel} color={a.color} onClick={onClick} />);
+                    }
                 }
                 return (undefined);
             })}
